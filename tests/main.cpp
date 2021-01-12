0a5deaebe5110042d57d4fc78810596041d4510d
Konstantin Kuznetsov <kkonsw@gmail.com>
2021-01-12 16:17:33 +0300

    Configure project structure

---
+++ tests/main.cpp
@@ -0,0 +1,4 @@
+// Copyright 2021 Kuznetsov Konstantin
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
---
+++ tests/test.cpp
@@ -0,0 +1,3 @@
+// Copyright 2021 Kuznetsov Konstantin
+
+#include <catch2/catch.hpp>
